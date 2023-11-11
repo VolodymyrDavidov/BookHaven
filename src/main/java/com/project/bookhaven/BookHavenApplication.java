f462314b5a2d02c8c36c09d6fc4648d3a4e72e81
Volodymyr <volodymyrdavidov1@gmail.com>
2023-11-11 19:51:37 +0200

    create Book model, repo and service

--- src/main/java/com/project/bookhaven/BookHavenApplication.java
+++ src/main/java/com/project/bookhaven/BookHavenApplication.java
@@ -1,13 +1,20 @@
 package com.project.bookhaven;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookHavenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookHavenApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner myCommandLineRunner() {
+        return args -> System.out.println("Hello from CommandLineRunner!");
     }
 
 }
