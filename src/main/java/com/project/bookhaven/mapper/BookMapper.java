c5ca924593f21db77761873f8b87506aaf5ce51c
Volodymyr <volodymyrdavidov1@gmail.com>
2023-11-15 22:46:27 +0200

    Add Spring JPA and Liquibase, impl soft delete concept, add method for update book, change ddl-auto to validate in app.properties

--- src/main/java/com/project/bookhaven/mapper/BookMapper.java
+++ src/main/java/com/project/bookhaven/mapper/BookMapper.java
@@ -5,10 +5,13 @@ import com.project.bookhaven.dto.BookDto;
 import com.project.bookhaven.dto.CreateBookRequestDto;
 import com.project.bookhaven.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
+
+    void updateFromDto(CreateBookRequestDto requestDto, @MappingTarget Book book);
 }
--- src/main/java/com/project/bookhaven/model/Book.java
+++ src/main/java/com/project/bookhaven/model/Book.java
@@ -1,22 +1,46 @@
 package com.project.bookhaven.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Title cannot be blank")
     private String title;
+    @NotBlank(message = "Author cannot be blank")
     private String author;
+    @NotNull
+    @Pattern(regexp = "^(978|979)-\\d{1,5}-\\d{2,7}-\\d{1,6}-[\\dX]$")
     private String isbn;
+    @Positive(message = "Price must be positive")
+    @DecimalMin(value = "0.01", inclusive = true, message = "Price must be greater than 0")
     private BigDecimal price;
+    @NotBlank(message = "Description cannot be blank")
     private String description;
+    @NotBlank(message = "CoverImage cannot be blank")
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
 }
