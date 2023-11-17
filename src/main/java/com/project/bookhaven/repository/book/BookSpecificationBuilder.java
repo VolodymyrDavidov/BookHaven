a37f58d7e1a3976d6f62ad458505f158364124df
Volodymyr <volodymyrdavidov1@gmail.com>
2023-11-17 14:39:33 +0200

    Add constants in BookSpecificationBuilder.java

--- src/main/java/com/project/bookhaven/repository/book/BookSpecificationBuilder.java
+++ src/main/java/com/project/bookhaven/repository/book/BookSpecificationBuilder.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private static final String TITLE = "title";
+    private static final String AUTHOR = "author";
     private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
 
     @Override
@@ -18,12 +20,12 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
         Specification<Book> specification = Specification.where(null);
         if (bookSearchParameters.titles() != null && bookSearchParameters.titles().length > 0) {
             specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("title")
+                    .getSpecificationProvider(TITLE)
                     .getSpecification(bookSearchParameters.titles()));
         }
         if (bookSearchParameters.authors() != null && bookSearchParameters.authors().length > 0) {
             specification = specification.and(bookSpecificationProviderManager
-                    .getSpecificationProvider("author")
+                    .getSpecificationProvider(AUTHOR)
                     .getSpecification(bookSearchParameters.authors()));
         }
         return specification;
