073baeffa98f3478a7ff2048864c19286f9ac5ec
Volodymyr <volodymyrdavidov1@gmail.com>
2023-11-20 16:25:41 +0200

    Add pagination, sorting and Swagger

--- src/main/java/com/project/bookhaven/service/BookService.java
+++ src/main/java/com/project/bookhaven/service/BookService.java
@@ -4,11 +4,12 @@ import com.project.bookhaven.dto.BookDto;
 import com.project.bookhaven.dto.BookSearchParameters;
 import com.project.bookhaven.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto bookRequestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
