2feba9c8b08fd9ec76eba2ca8f3fa8308d658808
VolodymyrDavidov <volodymyrdavidov1@gmail.com>
2023-12-11 12:40:33 +0200

    fix all comment

--- src/main/java/com/project/bookhaven/security/CurrentUser.java
+++ src/main/java/com/project/bookhaven/security/CurrentUser.java
@@ -1,10 +1,10 @@
 package com.project.bookhaven.security;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
